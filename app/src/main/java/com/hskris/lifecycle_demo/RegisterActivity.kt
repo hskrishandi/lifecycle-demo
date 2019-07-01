9021dafeb579aea38ef89a5301f95c6177c7590c
Hans <pt.gojekindonesia@PTs-MacBook-Pro-4.local>
2019-07-01 16:19:11 +0700

    Add logging for RegisterActivity lifecycles

--- app/src/main/java/com/hskris/lifecycle_demo/RegisterActivity.kt
+++ app/src/main/java/com/hskris/lifecycle_demo/RegisterActivity.kt
@@ -2,11 +2,42 @@ package com.hskris.lifecycle_demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class RegisterActivity : AppCompatActivity() {
+
+    companion object {
+        val TAG = "RegisterActivity"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        Log.d(TAG, "Enter onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "Enter onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "Enter onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "Enter onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "Enter onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "Enter onDestroy")
     }
 }
