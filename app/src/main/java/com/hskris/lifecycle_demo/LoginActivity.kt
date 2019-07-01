8f8e58f0ded2cfd7e1b5d029f4b0370700b74546
Hans <pt.gojekindonesia@PTs-MacBook-Pro-4.local>
2019-07-01 15:31:24 +0700

    Add tags for each lifecycle of LoginActivity

--- app/src/main/java/com/hskris/lifecycle_demo/LoginActivity.kt
+++ app/src/main/java/com/hskris/lifecycle_demo/LoginActivity.kt
@@ -2,11 +2,42 @@ package com.hskris.lifecycle_demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class LoginActivity : AppCompatActivity() {
 
+    companion object {
+        val TAG = "LoginActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "Enter onCreate")
         setContentView(R.layout.activity_login)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "Enter onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "Enter onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "Enter onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "Enter onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "Enter onDestroy")
     }
 }
